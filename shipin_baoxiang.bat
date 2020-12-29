68b99fce546839ff6f74217e29c0958522bf3b7d
Gdl <gdlbbxzsc@163.com>
2020-12-29 11:22:43 +0800

    s

---
+++ shipin_baoxiang.bat
@@ -0,0 +1,61 @@
+@echo off
+d:
+cd D:\dev\Android\sdk\platform-tools
+ 
+:chong_xin_kai_shi
+set m=
+
+:xun_huan_hua_dong
+
+rem echo »¬¶¯·­Ò³ 
+adb shell input swipe 999 900 999 138  
+
+set /a m+=1
+rem echo »¬¶¯·­Ò³´ÎÊý
+rem echo %n%
+
+rem echo ÔÝÍ£10Ãë
+rem TIMEOUT /T 15
+ping localhost -n 10 > nul 
+rem echo ---
+
+if %m% geq 120 ( 
+    goto :qu_bao_xiang
+)
+
+goto :xun_huan_hua_dong
+ 
+rem echo pause
+pause 
+
+:qu_bao_xiang
+
+rem echo È¥×¬Ç®
+adb shell input tap 549 1831
+
+ping localhost -n 4 > nul
+
+rem echo µã»÷±¦Ïä
+adb shell input tap 922 1746
+
+ping localhost -n 3 > nul
+
+rem echo ¹Ø±Õ±¦Ïä
+adb shell input tap 546 1374
+
+ping localhost -n 2 > nul
+
+rem echo ÍË³öÈ¥×¬Ç®
+adb shell input keyevent 4
+
+rem echo ----------------------
+
+goto :chong_xin_kai_shi
+
+
+
+
+
+
+
+
