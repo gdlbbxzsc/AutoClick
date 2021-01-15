c47a16986956585a0af1b3df6e86640378519b1f
Gdl <gdlbbxzsc@163.com>
2021-01-15 17:33:02 +0800

    s

--- app/src/main/java/com/pbph/autoclick/service/AutoClickAccessibilityService.java
+++ app/src/main/java/com/pbph/autoclick/service/AutoClickAccessibilityService.java
@@ -1,20 +1,10 @@
 package com.pbph.autoclick.service;
 
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.GestureDescription;
-import android.graphics.Path;
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.os.Build;
-import android.text.TextUtils;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.os.Parcelable;
 import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityNodeInfo;
-
-import androidx.annotation.RequiresApi;
-
-import java.security.InvalidParameterException;
-import java.util.ArrayList;
-import java.util.List;
 
 //https://blog.csdn.net/siyujiework/article/details/88996145 root权限下 自动授权无障碍
 //https://www.jianshu.com/p/4cd8c109cdfb 参考
@@ -36,12 +26,13 @@ public class AutoClickAccessibilityService extends AccessibilityService {
     //实现辅助功能
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        event.getAction();
-        event.getContentChangeTypes();
-        event.getEventType();
-        event.getEventTime();
 
+        int action = event.getAction();
+        int contentChangeTypes = event.getContentChangeTypes();
         int eventType = event.getEventType();
+        long eventTime = event.getEventTime();
+
+
         switch (eventType) {
             case AccessibilityEvent.TYPE_VIEW_CLICKED: {//界面点击
             }
@@ -49,7 +40,35 @@ public class AutoClickAccessibilityService extends AccessibilityService {
             case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED: {  //界面文字改动
             }
             break;
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED: {
+            }
+            break;
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED: {//通知
+                notification_wx(event);
+            }
+            break;
         }
+    }
+
+    private boolean notification_wx(AccessibilityEvent event) {
+        Parcelable parcelable = event.getParcelableData();
+        if (parcelable == null) return false;
+
+        if (!(parcelable instanceof Notification)) return false;
+
+        Notification notification = (Notification) parcelable;
+
+        String content = notification.tickerText.toString();
+
+        if (!(content.contains("[微信红包]"))) return false;
+
+        PendingIntent pendingIntent = notification.contentIntent;
+        try {
+            pendingIntent.send();
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 
 }
